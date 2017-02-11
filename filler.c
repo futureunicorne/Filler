192ad51ad48f4b76bc0d62651b31ff16598a801d
futureunicorne <elhadi-hamza@hotmail.fr>
2017-02-11 08:31:27 +0100

    begin

--- filler.c
+++ filler.c
@@ -6,15 +6,33 @@
 /*   By: hel-hadi <hel-hadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/10 18:26:09 by hel-hadi          #+#    #+#             */
-/*   Updated: 2017/02/10 19:03:50 by hel-hadi         ###   ########.fr       */
+/*   Updated: 2017/02/11 07:34:51 by hel-hadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/filler.h"
 
-int	recovery_place(int fd, char *str)
+int	ft_min_max(char *map, char *piece) //fonction d appel min max et fonction d etat actuelle
 {
-	fd = 0;
-	str = NULL;
+	int deep;
+
+	i = 0
+	if (ft_recursive_factorielle(m * m1))
+		deep = ++; 
+	return (0);
+}
+
+int	ft_eval_algo() // fonction d evalution
+{
+	return (0);
+}
+
+int ft_Ia_Min() // fonction  de lalgo min
+{
+	return (0);
+}
+
+int ft_Ia_Max( ) //
+{
 	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hel-hadi <hel-hadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/10 18:18:56 by hel-hadi          #+#    #+#             */
-/*   Updated: 2017/02/10 19:31:37 by hel-hadi         ###   ########.fr       */
+/*   Updated: 2017/02/11 07:42:18 by hel-hadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,29 @@
 int	main(void)
 {
 	int		fd;
+	int 	i;
 	char	*line1;
+	char	*line2;
+	char	*map;
+
 
 	fd = 0;
-	get_next_line(fd, &line1);
+	i = 0;
+	get_next_line(fd, &line1); // recuperation du nom de joueur et de la place
+	printf("%s\n", line1);
+	get_next_line(fd, &line2);  // recuperation de la taille de la map
+	printf("%s\n", line2);
+	while (1)
+	{
+		get_next_line(fd, &line)
+		// recuperation de la map
+		// recupperation de la piece
+		// algo min max
+		if (ft_check_strategy(map) == 0 && flag == 0)
+			ft_basic_algo(map, piece);
+		else if (ft_check_strategy(map) && flag == 0)
+			ft_min_max(map, piece);
+		i++;
+	}
 	return (0);
 }
